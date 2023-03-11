3af2b836b7dae4d8da6f85146fb54077dbbbaca0
anuu2802 <annug2802@gmail.com>
2023-03-12 00:01:24 +0530

    Added hello program

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+
+class Test
+{
+	public static void main(String [] args)
+	{
+		String s = "welcome";
+		System.out.println(" String s = " + s);
+		String s1 = new String("Students");
+		System.out.println("String s1 = " + s1);
+	}
+}
